373b61598eb860ee321628ebf7f4d165322a64e9
ottoshi <weeraphp@ais.co.th>
2022-05-13 10:25:05 +0700

    master

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
 <nav class="navbar navbar-light bg-light">
     <div class="container">
-        <a href="/"><span class="navbar-brand mb-0 h1">Todo</span></a>
+        <a href="/"><span class="navbar-brand mb-0 h1">TODO</span></a>
         <a href="/create"><span class="btn btn-primary">Create Todo</span></a> 
     </div> 
 
